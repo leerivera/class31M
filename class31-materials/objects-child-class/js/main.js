e6cfc4ba857d8c168ad741555cc61b7f6259a7df
leerivera <leerivera80@gmail.com>
2022-05-03 20:12:41 -0400

    hawk

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,25 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this.breed = breed
+    } 
+}
+
+class Hawk extends Animal{
+    constructor(name, breed){
+        super(name)
+        this.breed = breed
+    }
+}
+let falcony = new Hawk('Falcony', "Hawk")
+let simba = new Dog('Simba', 'Sheperd')
